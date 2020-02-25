783365d87e017ac651bbe2c3598fb0ef28cee3a7
Andrzej Iwanski <iwanski@fev.com>
2020-02-25 11:22:34 +0100

    create complete app.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {connect} from "react-redux";
+import Counter from "./Counter";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Powiazanie stanu z wlasciwoscia komponentu
+function mapStateToProps(state) {
+    return {
+        countValue: state.count
+    };
 }
 
-export default App;
+// Akcje
+let increaseAction = {type: "increase"};
+let decreaseAction = {type: "decrease"};
+
+// Powiazanie akcji z wlasciwosciami komponentu
+function mapDispatchToProps(dispatch) {
+    return {
+        increaseCount: function () {
+            return dispatch(increaseAction);
+        },
+        decreaseCount: function () {
+            return dispatch(decreaseAction);
+        }
+    };
+}
+
+// Komponent wyzszego rzedu
+const connectedComponent = connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(Counter);
+
+export default connectedComponent;
